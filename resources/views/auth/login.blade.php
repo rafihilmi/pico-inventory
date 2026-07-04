1cd5f6dc8652abea618e7aef29e3234f7ff1c94b
Muhammad Rafi Hilmi <rafihilmi40@gmail.com>
2026-07-04 12:08:13 +0700

    Add favicon using logo pico

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Pico Inventory</title>
+    <link rel="icon" type="image/jpeg" href="{{ asset('logo.jpeg') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
